9222c77bf88c62c39602c19b8a19322b2086ef4f
jongsikk <xxx_@kakao.com>
2024-04-11 10:33:41 +0900

    Refactor: timezone GMT+9 변경

--- src/main/java/com/nhnacademy/sensordata/service/impl/HumidityServiceImpl.java
+++ src/main/java/com/nhnacademy/sensordata/service/impl/HumidityServiceImpl.java
@@ -13,8 +13,11 @@ import org.springframework.data.influxdb.InfluxDBTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -30,20 +33,35 @@ public class HumidityServiceImpl implements HumidityService {
         QueryResult queryResult = influxDBTemplate.query(query);
 
         InfluxDBResultMapper resultMapper = new InfluxDBResultMapper();
-        return resultMapper.toPOJO(queryResult, Humidity.class).get(0);
+
+        Humidity humidity = resultMapper.toPOJO(queryResult, Humidity.class).get(0);
+
+        if (Objects.nonNull(humidity)) {
+            humidity.setTime(humidity.getTime().plus(9, ChronoUnit.HOURS));
+        }
+
+        return humidity;
     }
 
     @Override
     public List<HumidityMaxMin> getDailyHumidity() {
         LocalDate today = LocalDate.now();
-        Query query = QueryBuilder.newQuery(String.format("select * from hourly_extreme_humidity where time >= '%sT00:00:00Z' AND time <= '%sT00:00:00Z'", today, today.plusDays(1)))
+        Query query = QueryBuilder.newQuery(String.format("select * from hourly_extreme_humidity where time >= '%sT15:00:00Z' AND time <= '%sT15:00:00Z'", today.minusDays(1), today))
                 .forDatabase("tig")
                 .create();
 
         QueryResult queryResult = influxDBTemplate.query(query);
 
         InfluxDBResultMapper resultMapper = new InfluxDBResultMapper();
+
         List<HumidityMaxMin> humidityMaxMinList = resultMapper.toPOJO(queryResult, HumidityMaxMin.class);
+        humidityMaxMinList = humidityMaxMinList.stream()
+                .peek(humidity -> {
+                    if (Objects.nonNull(humidity)) {
+                        humidity.setTime(humidity.getTime().plus(9, ChronoUnit.HOURS));
+                    }
+                })
+                .collect(Collectors.toList());
 
         return humidityMaxMinList.isEmpty() ? Collections.emptyList() : humidityMaxMinList;
     }
