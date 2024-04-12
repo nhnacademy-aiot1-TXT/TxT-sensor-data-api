dc4d6c31b16396c82e3c687c81fc634c798ec582
pass0210 <pass0210@gmail.com>
2024-04-12 13:49:20 +0900

    Refactor: InfluxDBUtil로 리팩토링
    query작성과 query result를 반환하는 부분을 util로 묶음

--- src/main/java/com/nhnacademy/sensordata/service/impl/TemperatureServiceImpl.java
+++ src/main/java/com/nhnacademy/sensordata/service/impl/TemperatureServiceImpl.java
@@ -5,13 +5,10 @@ import com.nhnacademy.sensordata.entity.TemperatureMaxMinDaily;
 import com.nhnacademy.sensordata.entity.TemperatureMaxMinMonthly;
 import com.nhnacademy.sensordata.entity.TemperatureMaxMinWeekly;
 import com.nhnacademy.sensordata.service.TemperatureService;
+import com.nhnacademy.sensordata.utils.InfluxDBUtil;
 import lombok.RequiredArgsConstructor;
-import org.influxdb.dto.BoundParameterQuery.QueryBuilder;
-import org.influxdb.dto.Point;
-import org.influxdb.dto.Query;
 import org.influxdb.dto.QueryResult;
 import org.influxdb.impl.InfluxDBResultMapper;
-import org.springframework.data.influxdb.InfluxDBTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -24,16 +21,13 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class TemperatureServiceImpl implements TemperatureService {
-    private final InfluxDBTemplate<Point> influxDBTemplate;
+    private final InfluxDBUtil influxDBUtil;
     private final InfluxDBResultMapper resultMapper;
 
     @Override
     public Temperature getTemperature() {
-        Query query = QueryBuilder.newQuery("select time, device, place, topic, value from temperature order by time desc limit 1")
-                .forDatabase("tig")
-                .create();
-
-        QueryResult queryResult = influxDBTemplate.query(query);
+        String query = "select time, device, place, topic, value from temperature order by time desc limit 1";
+        QueryResult queryResult = influxDBUtil.processingQuery(query);
 
         Temperature temperature = resultMapper.toPOJO(queryResult, Temperature.class).get(0);
         if (Objects.nonNull(temperature)) {
@@ -46,12 +40,8 @@ public class TemperatureServiceImpl implements TemperatureService {
     @Override
     public List<TemperatureMaxMinDaily> getDailyTemperatures() {
         LocalDate today = LocalDate.now();
-
-        Query query = QueryBuilder.newQuery(String.format("SELECT time, max_temperature, min_temperature FROM temperature_hourly WHERE time >= '%sT15:00:00Z' AND time < '%sT15:00:00Z'", today.minusDays(1), today))
-                .forDatabase("tig")
-                .create();
-
-        QueryResult queryResult = influxDBTemplate.query(query);
+        String query = String.format("SELECT time, max_temperature, min_temperature FROM temperature_hourly WHERE time >= '%sT15:00:00Z' AND time < '%sT15:00:00Z'", today.minusDays(1), today);
+        QueryResult queryResult = influxDBUtil.processingQuery(query);
 
         List<TemperatureMaxMinDaily> temperatures = resultMapper.toPOJO(queryResult, TemperatureMaxMinDaily.class);
 
@@ -70,20 +60,14 @@ public class TemperatureServiceImpl implements TemperatureService {
     @Override
     public List<TemperatureMaxMinWeekly> getWeeklyTemperatures() {
         LocalDate today = LocalDate.now();
+        String dailyQuery = String.format("SELECT time, max_temperature, min_temperature FROM temperature_daily WHERE time >= '%sT15:00:00Z' AND time < '%sT15:00:00Z'", today.minusWeeks(1), today);
+        String hourlyQuery = "SELECT time, max_temperature, min_temperature FROM temperature_hourly order by time desc limit 1";
 
-        Query query = QueryBuilder.newQuery(String.format("SELECT time, max_temperature, min_temperature FROM temperature_daily WHERE time >= '%sT15:00:00Z' AND time < '%sT15:00:00Z'", today.minusWeeks(1), today))
-                .forDatabase("tig")
-                .create();
+        QueryResult dailyQueryResult = influxDBUtil.processingQuery(dailyQuery);
+        QueryResult hourlyQueryResult = influxDBUtil.processingQuery(hourlyQuery);
 
-        Query query2 = QueryBuilder.newQuery("SELECT time, max_temperature, min_temperature FROM temperature_hourly order by time desc limit 1")
-                .forDatabase("tig")
-                .create();
-
-        QueryResult queryResult = influxDBTemplate.query(query);
-        QueryResult queryResult2 = influxDBTemplate.query(query2);
-
-        List<TemperatureMaxMinWeekly> temperatures = resultMapper.toPOJO(queryResult, TemperatureMaxMinWeekly.class);
-        TemperatureMaxMinDaily temperatureMaxMinDaily = resultMapper.toPOJO(queryResult2, TemperatureMaxMinDaily.class).get(0);
+        List<TemperatureMaxMinWeekly> temperatures = resultMapper.toPOJO(dailyQueryResult, TemperatureMaxMinWeekly.class);
+        TemperatureMaxMinDaily temperatureMaxMinDaily = resultMapper.toPOJO(hourlyQueryResult, TemperatureMaxMinDaily.class).get(0);
 
         temperatures = temperatures.stream()
                 .map(temperature -> new TemperatureMaxMinWeekly(
@@ -109,20 +93,14 @@ public class TemperatureServiceImpl implements TemperatureService {
     @Override
     public List<TemperatureMaxMinMonthly> getMonthlyTemperatures() {
         LocalDate today = LocalDate.now();
+        String dailyQuery = String.format("SELECT time, max_temperature, min_temperature FROM temperature_daily WHERE time >= '%sT15:00:00Z' AND time < '%sT15:00:00Z'", today.minusMonths(1), today);
+        String hourlyQuery = "SELECT time, max_temperature, min_temperature FROM temperature_hourly order by time desc limit 1";
 
-        Query query = QueryBuilder.newQuery(String.format("SELECT time, max_temperature, min_temperature FROM temperature_daily WHERE time >= '%sT15:00:00Z' AND time < '%sT15:00:00Z'", today.minusMonths(1), today))
-                .forDatabase("tig")
-                .create();
+        QueryResult dailyQueryResult = influxDBUtil.processingQuery(dailyQuery);
+        QueryResult hourlyQueryResult = influxDBUtil.processingQuery(hourlyQuery);
 
-        Query query2 = QueryBuilder.newQuery("SELECT time, max_temperature, min_temperature FROM temperature_hourly order by time desc limit 1")
-                .forDatabase("tig")
-                .create();
-
-        QueryResult queryResult = influxDBTemplate.query(query);
-        QueryResult queryResult2 = influxDBTemplate.query(query2);
-
-        List<TemperatureMaxMinMonthly> temperatures = resultMapper.toPOJO(queryResult, TemperatureMaxMinMonthly.class);
-        TemperatureMaxMinDaily temperatureMaxMinDaily = resultMapper.toPOJO(queryResult2, TemperatureMaxMinDaily.class).get(0);
+        List<TemperatureMaxMinMonthly> temperatures = resultMapper.toPOJO(dailyQueryResult, TemperatureMaxMinMonthly.class);
+        TemperatureMaxMinDaily temperatureMaxMinDaily = resultMapper.toPOJO(hourlyQueryResult, TemperatureMaxMinDaily.class).get(0);
 
         temperatures = temperatures.stream()
                 .map(temperature -> new TemperatureMaxMinMonthly(
