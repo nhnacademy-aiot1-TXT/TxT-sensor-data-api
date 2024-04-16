1af5a5f8565d69fce7c60153da5e7bb42aae1049
jongsikk <xxx_@kakao.com>
2024-04-16 21:54:51 +0900

    Refactor: InfluxDB 버전변경 Co2 api 수정

--- src/main/java/com/nhnacademy/sensordata/service/impl/Co2ServiceImpl.java
+++ src/main/java/com/nhnacademy/sensordata/service/impl/Co2ServiceImpl.java
@@ -1,127 +1,134 @@
 package com.nhnacademy.sensordata.service.impl;
 
+import com.influxdb.client.InfluxDBClient;
+import com.influxdb.query.dsl.Flux;
 import com.nhnacademy.sensordata.entity.co2.Co2;
 import com.nhnacademy.sensordata.entity.co2.Co2MaxMinDaily;
 import com.nhnacademy.sensordata.entity.co2.Co2MaxMinMonthly;
 import com.nhnacademy.sensordata.entity.co2.Co2MaxMinWeekly;
 import com.nhnacademy.sensordata.service.Co2Service;
-import com.nhnacademy.sensordata.utils.InfluxDBUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
+
+import static com.influxdb.query.dsl.functions.restriction.Restrictions.*;
 
 @Service
 @RequiredArgsConstructor
 public class Co2ServiceImpl implements Co2Service {
-    private final InfluxDBUtil influxDBUtil;
+    private final InfluxDBClient influxDBClient;
 
     @Override
     public Co2 getCo2() {
-//        List<FluxTable> tables = influxDBUtil.processingQuery("from(bucket: \"TxT-iot\")\n" +
-//                "  |> range(start: -1d)\n" +
-//                "  |> filter(fn: (r) => r._measurement == \"co2\")\n" +
-//                "  |> sort(columns: [\"_time\"], desc: true)\n" +
-//                "  |> limit(n: 1)");
-//        Co2 co2Result = tables.stream()
-//                .flatMap(table -> table.getRecords().stream())
-//                .map(record -> {
-//                    Co2 co2 = new Co2();
-//                    co2.setTime(record.getTime());
-//                    co2.setDevice(record.get);
-//                    co2.setPlace((String) record.getValueByKey("place"));
-//                    co2.setTopic((String) record.getValueByKey("topic"));
-//                    co2.setValue((Integer) record.getValueByKey("value"));
-//                    return co2;
-//                })
-//                .collect(Collectors.toList())
-//                .get(0);
-//
-//        if (Objects.nonNull(co2Result)) {
-//            co2Result.setTime(co2Result.getTime().plus(9, ChronoUnit.HOURS));
-//        }
-//
-//        return co2Result;
-        return null;
+        Flux fluxQuery = Flux.from("TxT-iot")
+                .range(-1L, ChronoUnit.MINUTES)
+                .filter(measurement().equal("co2"))
+                .filter(or(
+                        field().equal("device"),
+                        field().equal("place"),
+                        field().equal("topic"),
+                        field().equal("value")
+                ))
+                .last()
+                .pivot()
+                .withRowKey(new String[]{"_time"})
+                .withColumnKey(new String[]{"_field"})
+                .withValueColumn("_value")
+                .map("({ r with value: float(v: r.value)})")
+                .timeShift(9L, ChronoUnit.HOURS);
+
+        return influxDBClient.getQueryApi().query(fluxQuery.toString(), Co2.class).stream().findFirst().orElse(null);
     }
 
     @Override
     public List<Co2MaxMinDaily> getDailyCo2() {
-        LocalDate today = LocalDate.now();
+        Instant startTime = Instant.parse(String.format("%sT15:00:00Z", LocalDate.now().minusDays(1)));
+        Instant endTime = Instant.now();
 
-//        QueryResult queryResult = influxDBUtil.processingQuery(String.format("select * from co2_hourly where time >= '%sT15:00:00Z' AND time < '%sT15:00:00Z'", today.minusDays(1), today));
-//
-//        List<Co2MaxMinDaily> co2MaxMinList = resultMapper.toPOJO(queryResult, Co2MaxMinDaily.class);
-//
-//        co2MaxMinList = co2MaxMinList.stream()
-//                .map(co2 -> new Co2MaxMinDaily(
-//                                co2.getTime().plus(9, ChronoUnit.HOURS),
-//                                co2.getMaxCo2(),
-//                                co2.getMinCo2()
-//                        )
-//                )
-//                .collect(Collectors.toList());
-//
-//        return co2MaxMinList.isEmpty() ? Collections.emptyList() : co2MaxMinList;
-        return null;
+        Flux query = Flux.from("TxT-iot-old")
+                .range(startTime, endTime)
+                .filter(measurement().equal("co2_hourly"))
+                .filter(or(
+                        field().equal("max_co2"),
+                        field().equal("min_co2")
+                ))
+                .pivot()
+                .withRowKey(new String[]{"_time"})
+                .withColumnKey(new String[]{"_field"})
+                .withValueColumn("_value")
+                .timeShift(9L, ChronoUnit.HOURS);
+
+        return influxDBClient.getQueryApi().query(query.toString(), Co2MaxMinDaily.class);
     }
 
     @Override
     public List<Co2MaxMinWeekly> getWeeklyCo2() {
-        LocalDate today = LocalDate.now();
+        Instant startTime = Instant.parse(String.format("%sT15:00:00Z", LocalDate.now().minusWeeks(1)));
+        Instant endTime = Instant.now();
 
-//        QueryResult queryResult = influxDBUtil.processingQuery(String.format("select * from co2_daily where time >= '%sT15:00:00Z' AND time < '%sT15:00:00Z'", today.minusWeeks(1), today));
-//        QueryResult queryResult2 = influxDBUtil.processingQuery("select * from co2_hourly order by time desc");
-//
-//        List<Co2MaxMinWeekly> co2MaxMinList = resultMapper.toPOJO(queryResult, Co2MaxMinWeekly.class);
-//        Co2MaxMinDaily co2LastHour = resultMapper.toPOJO(queryResult2, Co2MaxMinDaily.class).get(0);
-//
-//        co2MaxMinList = co2MaxMinList.stream()
-//                .map(co2 -> new Co2MaxMinWeekly(
-//                        co2.getTime().plus(9, ChronoUnit.HOURS),
-//                        co2.getMaxCo2(),
-//                        co2.getMinCo2()
-//                ))
-//                .collect(Collectors.toList());
-//
-//        if (Objects.nonNull(co2LastHour)) {
-//            co2MaxMinList.add(new Co2MaxMinWeekly(
-//                    co2LastHour.getTime().plus(9, ChronoUnit.HOURS),
-//                    co2LastHour.getMaxCo2(),
-//                    co2LastHour.getMinCo2()));
-//        }
-//
-//        return co2MaxMinList.isEmpty() ? Collections.emptyList() : co2MaxMinList;
-        return null;
+        Flux query = Flux.from("TxT-iot-old")
+                .range(startTime, endTime)
+                .filter(measurement().equal("co2_daily"))
+                .filter(or(
+                        field().equal("max_co2"),
+                        field().equal("min_co2")
+                ))
+                .pivot()
+                .withRowKey(new String[]{"_time"})
+                .withColumnKey(new String[]{"_field"})
+                .withValueColumn("_value")
+                .timeShift(9L, ChronoUnit.HOURS);
+        Flux lastHourQuery = Flux.from("TxT-iot-old")
+                .range(-1L, ChronoUnit.DAYS)
+                .filter(measurement().equal("co2_hourly"))
+                .last()
+                .pivot()
+                .withRowKey(new String[]{"_time"})
+                .withColumnKey(new String[]{"_field"})
+                .withValueColumn("_value")
+                .timeShift(9L, ChronoUnit.HOURS);
+
+        List<Co2MaxMinWeekly> weeklyList = influxDBClient.getQueryApi().query(query.toString(), Co2MaxMinWeekly.class);
+        Co2MaxMinWeekly lastHour = influxDBClient.getQueryApi().query(lastHourQuery.toString(), Co2MaxMinWeekly.class).get(0);
+        weeklyList.add(new Co2MaxMinWeekly(lastHour.getTime(), lastHour.getMaxCo2(), lastHour.getMinCo2()));
+        return weeklyList;
     }
 
     @Override
     public List<Co2MaxMinMonthly> getMonthlyCo2() {
-        LocalDate today = LocalDate.now();
+        Instant startTime = Instant.parse(String.format("%sT15:00:00Z", LocalDate.now().minusWeeks(1)));
+        Instant endTime = Instant.now();
 
-//        QueryResult queryResult = influxDBUtil.processingQuery(String.format("select * from co2_daily where time >= '%sT15:00:00Z' AND time < '%sT15:00:00Z'", today.minusWeeks(1), today));
-//        QueryResult queryResult2 = influxDBUtil.processingQuery("select * from co2_hourly order by time desc");
-//
-//        List<Co2MaxMinMonthly> co2MaxMinList = resultMapper.toPOJO(queryResult, Co2MaxMinMonthly.class);
-//        Co2MaxMinDaily co2LastHour = resultMapper.toPOJO(queryResult2, Co2MaxMinDaily.class).get(0);
-//
-//        co2MaxMinList = co2MaxMinList.stream()
-//                .map(co2 -> new Co2MaxMinMonthly(
-//                        co2.getTime().plus(9, ChronoUnit.HOURS),
-//                        co2.getMaxCo2(),
-//                        co2.getMinCo2()
-//                ))
-//                .collect(Collectors.toList());
-//
-//        if (Objects.nonNull(co2LastHour)) {
-//            co2MaxMinList.add(new Co2MaxMinMonthly(
-//                    co2LastHour.getTime().plus(9, ChronoUnit.HOURS),
-//                    co2LastHour.getMaxCo2(),
-//                    co2LastHour.getMinCo2()));
-//        }
-//
-//        return co2MaxMinList.isEmpty() ? Collections.emptyList() : co2MaxMinList;
-        return null;
+        Flux query = Flux.from("TxT-iot-old")
+                .range(startTime, endTime)
+                .filter(measurement().equal("co2_daily"))
+                .filter(or(
+                        field().equal("max_co2"),
+                        field().equal("min_co2")
+                ))
+                .pivot()
+                .withRowKey(new String[]{"_time"})
+                .withColumnKey(new String[]{"_field"})
+                .withValueColumn("_value")
+                .timeShift(9L, ChronoUnit.HOURS);
+
+        Flux lastHourQuery = Flux.from("TxT-iot-old")
+                .range(-1L, ChronoUnit.DAYS)
+                .filter(measurement().equal("co2_hourly"))
+                .last()
+                .pivot()
+                .withRowKey(new String[]{"_time"})
+                .withColumnKey(new String[]{"_field"})
+                .withValueColumn("_value")
+                .timeShift(9L, ChronoUnit.HOURS);
+
+        List<Co2MaxMinMonthly> monthlyList = influxDBClient.getQueryApi().query(query.toString(), Co2MaxMinMonthly.class);
+        Co2MaxMinDaily lastHour = influxDBClient.getQueryApi().query(lastHourQuery.toString(), Co2MaxMinDaily.class).get(0);
+        monthlyList.add(new Co2MaxMinMonthly(lastHour.getTime(), lastHour.getMaxCo2(), lastHour.getMinCo2()));
+        return monthlyList;
     }
 }
--- src/test/java/com/nhnacademy/sensordata/service/Co2ServiceImplTest.java
+++ src/test/java/com/nhnacademy/sensordata/service/Co2ServiceImplTest.java
@@ -1,127 +1,130 @@
-//package com.nhnacademy.sensordata.service;
-//
-//import com.influxdb.client.InfluxDBClient;
-//import com.nhnacademy.sensordata.entity.co2.Co2;
-//import com.nhnacademy.sensordata.entity.co2.Co2MaxMinDaily;
-//import com.nhnacademy.sensordata.entity.co2.Co2MaxMinMonthly;
-//import com.nhnacademy.sensordata.entity.co2.Co2MaxMinWeekly;
-//import org.junit.jupiter.api.Disabled;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//
-//import java.time.Instant;
-//import java.time.temporal.ChronoUnit;
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.assertAll;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.ArgumentMatchers.eq;
-//import static org.mockito.BDDMockito.given;
-//
-//@Disabled
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-//class Co2ServiceImplTest {
-//    @Autowired
-//    private Co2Service co2Service;
-//    @MockBean
-//    private InfluxDBClient influxDBClient;
-//
-//    @Test
-//    void getCo2() {
-//        Instant time = Instant.now();
-//        String device = "test device";
-//        String place = "test place";
-//        String topic = "test topic";
-//        Integer value = 20;
-//        Co2 co2 = new Co2(time, device, place, topic, value);
-//
-//        given(influxDBUtil.processingQuery(any())).willReturn(new QueryResult());
-//        given(resultMapper.toPOJO(any(), any())).willReturn(List.of(co2));
-//
-//        Co2 resultCo2 = co2Service.getCo2();
-//
-//        assertAll(
-//                () -> assertEquals(co2.getTime(), resultCo2.getTime()),
-//                () -> assertEquals(co2.getDevice(), resultCo2.getDevice()),
-//                () -> assertEquals(co2.getPlace(), resultCo2.getPlace()),
-//                () -> assertEquals(co2.getTopic(), resultCo2.getTopic()),
-//                () -> assertEquals(co2.getValue(), resultCo2.getValue())
-//        );
-//    }
-//
-//    @Test
-//    void getDailyCo2() {
-//        Instant time = Instant.now();
-//        Integer maxCo2 = 80;
-//        Integer minCo2 = 60;
-//        Co2MaxMinDaily co2Daily = new Co2MaxMinDaily(time, maxCo2, minCo2);
-//
-//        given(influxDBUtil.processingQuery(any())).willReturn(new QueryResult());
-//        given(resultMapper.toPOJO(any(), any())).willReturn(List.of(co2Daily));
-//
-//        Co2MaxMinDaily resultCo2 = co2Service.getDailyCo2().get(0);
-//
-//        assertAll(
-//                () -> assertEquals(co2Daily.getTime().plus(9, ChronoUnit.HOURS), resultCo2.getTime()),
-//                () -> assertEquals(co2Daily.getMaxCo2(), resultCo2.getMaxCo2()),
-//                () -> assertEquals(co2Daily.getMinCo2(), resultCo2.getMinCo2())
-//        );
-//    }
-//
-//    @Test
-//    void getWeeklyCo2() {
-//        Instant time = Instant.now();
-//        Integer weeklyMaxCo2 = 80;
-//        Integer weeklyMinCo2 = 60;
-//        Integer dailyMaxCo2 = 80;
-//        Integer dailyMinCo2 = 60;
-//        Co2MaxMinWeekly co2Weekly = new Co2MaxMinWeekly(time, weeklyMaxCo2, weeklyMinCo2);
-//        Co2MaxMinDaily co2Daily = new Co2MaxMinDaily(time, dailyMaxCo2, dailyMinCo2);
-//
-//        given(influxDBUtil.processingQuery(any())).willReturn(new QueryResult());
-//        given(resultMapper.toPOJO(any(), eq(Co2MaxMinWeekly.class))).willReturn(List.of(co2Weekly));
-//        given(resultMapper.toPOJO(any(), eq(Co2MaxMinDaily.class))).willReturn(List.of(co2Daily));
-//
-//        List<Co2MaxMinWeekly> resultCo2 = co2Service.getWeeklyCo2();
-//
-//        assertAll(
-//                () -> assertEquals(co2Weekly.getTime().plus(9, ChronoUnit.HOURS), resultCo2.get(0).getTime()),
-//                () -> assertEquals(co2Daily.getTime().plus(9, ChronoUnit.HOURS), resultCo2.get(1).getTime()),
-//                () -> assertEquals(co2Weekly.getMaxCo2(), resultCo2.get(0).getMaxCo2()),
-//                () -> assertEquals(co2Weekly.getMinCo2(), resultCo2.get(0).getMinCo2()),
-//                () -> assertEquals(co2Daily.getMaxCo2(), resultCo2.get(1).getMaxCo2()),
-//                () -> assertEquals(co2Daily.getMinCo2(), resultCo2.get(1).getMinCo2())
-//        );
-//    }
-//
-//    @Test
-//    void getMonthlyCo2() {
-//        Instant time = Instant.now();
-//        Integer monthlyMaxCo2 = 80;
-//        Integer monthlyMinCo2 = 60;
-//        Integer dailyMaxCo2 = 80;
-//        Integer dailyMinCo2 = 60;
-//        Co2MaxMinMonthly co2Weekly = new Co2MaxMinMonthly(time, monthlyMaxCo2, monthlyMinCo2);
-//        Co2MaxMinDaily co2Daily = new Co2MaxMinDaily(time, dailyMaxCo2, dailyMinCo2);
-//
-//        given(influxDBUtil.processingQuery(any())).willReturn(new QueryResult());
-//        given(resultMapper.toPOJO(any(), eq(Co2MaxMinMonthly.class))).willReturn(List.of(co2Weekly));
-//        given(resultMapper.toPOJO(any(), eq(Co2MaxMinDaily.class))).willReturn(List.of(co2Daily));
-//
-//        List<Co2MaxMinMonthly> resultCo2 = co2Service.getMonthlyCo2();
-//
-//        assertAll(
-//                () -> assertEquals(co2Weekly.getTime().plus(9, ChronoUnit.HOURS), resultCo2.get(0).getTime()),
-//                () -> assertEquals(co2Daily.getTime().plus(9, ChronoUnit.HOURS), resultCo2.get(1).getTime()),
-//                () -> assertEquals(co2Weekly.getMaxCo2(), resultCo2.get(0).getMaxCo2()),
-//                () -> assertEquals(co2Weekly.getMinCo2(), resultCo2.get(0).getMinCo2()),
-//                () -> assertEquals(co2Daily.getMaxCo2(), resultCo2.get(1).getMaxCo2()),
-//                () -> assertEquals(co2Daily.getMinCo2(), resultCo2.get(1).getMinCo2())
-//        );
-//    }
-//
-//
-//}
+package com.nhnacademy.sensordata.service;
+
+import com.influxdb.client.InfluxDBClient;
+import com.influxdb.client.QueryApi;
+import com.nhnacademy.sensordata.entity.co2.Co2;
+import com.nhnacademy.sensordata.entity.co2.Co2MaxMinDaily;
+import com.nhnacademy.sensordata.entity.co2.Co2MaxMinMonthly;
+import com.nhnacademy.sensordata.entity.co2.Co2MaxMinWeekly;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+class Co2ServiceImplTest {
+    @Autowired
+    private Co2Service co2Service;
+    @MockBean
+    private InfluxDBClient influxDBClient;
+    @Mock
+    private QueryApi queryApi;
+
+    @Test
+    void getCo2() {
+        Instant time = Instant.now();
+        String device = "test device";
+        String place = "test place";
+        String topic = "test topic";
+        Integer value = 20;
+        Co2 co2 = new Co2(time, device, place, topic, value);
+
+        given(influxDBClient.getQueryApi()).willReturn(queryApi);
+        given(queryApi.query(anyString(), eq(Co2.class))).willReturn(List.of(co2));
+
+        Co2 resultCo2 = co2Service.getCo2();
+
+        assertAll(
+                () -> assertEquals(co2.getTime(), resultCo2.getTime()),
+                () -> assertEquals(co2.getDevice(), resultCo2.getDevice()),
+                () -> assertEquals(co2.getPlace(), resultCo2.getPlace()),
+                () -> assertEquals(co2.getTopic(), resultCo2.getTopic()),
+                () -> assertEquals(co2.getValue(), resultCo2.getValue())
+        );
+    }
+
+    @Test
+    void getDailyCo2() {
+        Instant time = Instant.now();
+        Integer maxCo2 = 80;
+        Integer minCo2 = 60;
+        Co2MaxMinDaily co2Daily = new Co2MaxMinDaily(time, maxCo2, minCo2);
+
+        given(influxDBClient.getQueryApi()).willReturn(queryApi);
+        given(queryApi.query(anyString(), eq(Co2MaxMinDaily.class))).willReturn(List.of(co2Daily));
+
+        Co2MaxMinDaily resultCo2 = co2Service.getDailyCo2().get(0);
+
+        assertAll(
+                () -> assertEquals(co2Daily.getTime(), resultCo2.getTime()),
+                () -> assertEquals(co2Daily.getMaxCo2(), resultCo2.getMaxCo2()),
+                () -> assertEquals(co2Daily.getMinCo2(), resultCo2.getMinCo2())
+        );
+    }
+
+    @Test
+    void getWeeklyCo2() {
+        Instant time = Instant.now();
+        Integer weeklyMaxCo2 = 80;
+        Integer weeklyMinCo2 = 60;
+        Integer dailyMaxCo2 = 80;
+        Integer dailyMinCo2 = 60;
+        Co2MaxMinWeekly co2Weekly = new Co2MaxMinWeekly(time, weeklyMaxCo2, weeklyMinCo2);
+        Co2MaxMinDaily co2Daily = new Co2MaxMinDaily(time, dailyMaxCo2, dailyMinCo2);
+
+        given(influxDBClient.getQueryApi()).willReturn(queryApi);
+        given(queryApi.query(anyString(), eq(Co2MaxMinWeekly.class))).willReturn(new ArrayList<>(List.of(co2Weekly)));
+        given(queryApi.query(anyString(), eq(Co2MaxMinDaily.class))).willReturn(new ArrayList<>(List.of(co2Daily)));
+
+        List<Co2MaxMinWeekly> resultCo2 = co2Service.getWeeklyCo2();
+
+        assertAll(
+                () -> assertEquals(co2Weekly.getTime(), resultCo2.get(0).getTime()),
+                () -> assertEquals(co2Daily.getTime(), resultCo2.get(1).getTime()),
+                () -> assertEquals(co2Weekly.getMaxCo2(), resultCo2.get(0).getMaxCo2()),
+                () -> assertEquals(co2Weekly.getMinCo2(), resultCo2.get(0).getMinCo2()),
+                () -> assertEquals(co2Daily.getMaxCo2(), resultCo2.get(1).getMaxCo2()),
+                () -> assertEquals(co2Daily.getMinCo2(), resultCo2.get(1).getMinCo2())
+        );
+    }
+
+    @Test
+    void getMonthlyCo2() {
+        Instant time = Instant.now();
+        Integer monthlyMaxCo2 = 80;
+        Integer monthlyMinCo2 = 60;
+        Integer dailyMaxCo2 = 80;
+        Integer dailyMinCo2 = 60;
+        Co2MaxMinMonthly co2Monthly = new Co2MaxMinMonthly(time, monthlyMaxCo2, monthlyMinCo2);
+        Co2MaxMinDaily co2Daily = new Co2MaxMinDaily(time, dailyMaxCo2, dailyMinCo2);
+
+        given(influxDBClient.getQueryApi()).willReturn(queryApi);
+        given(queryApi.query(anyString(), eq(Co2MaxMinMonthly.class))).willReturn(new ArrayList<>(List.of(co2Monthly)));
+        given(queryApi.query(anyString(), eq(Co2MaxMinDaily.class))).willReturn(new ArrayList<>(List.of(co2Daily)));
+
+        List<Co2MaxMinMonthly> resultCo2 = co2Service.getMonthlyCo2();
+
+        assertAll(
+                () -> assertEquals(co2Monthly.getTime(), resultCo2.get(0).getTime()),
+                () -> assertEquals(co2Daily.getTime(), resultCo2.get(1).getTime()),
+                () -> assertEquals(co2Monthly.getMaxCo2(), resultCo2.get(0).getMaxCo2()),
+                () -> assertEquals(co2Monthly.getMinCo2(), resultCo2.get(0).getMinCo2()),
+                () -> assertEquals(co2Daily.getMaxCo2(), resultCo2.get(1).getMaxCo2()),
+                () -> assertEquals(co2Daily.getMinCo2(), resultCo2.get(1).getMinCo2())
+        );
+    }
+
+
+}
