39e6f2ca4e3cee3fb675413b340e1b29e8b97bc5
pass0210 <pass0210@gmail.com>
2024-04-17 21:35:03 +0900

    Docs: IlluminationRestController JavaDoc 작성
    ### 작업 내용
    - 조도 단일 조회 설명 작성
    - 조도 일간 조회 설명 작성
    - 조도 주간 조회 설명 작성
    - 조도 월간 조회 설명 작성

--- src/main/java/com/nhnacademy/sensordata/controller/IlluminationRestController.java
+++ src/main/java/com/nhnacademy/sensordata/controller/IlluminationRestController.java
@@ -13,12 +13,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+/**
+ * Illumination api controller
+ *
+ * @author parksangwon
+ * @version 1.0.0
+ */
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/illumination")
 public class IlluminationRestController {
     private final IlluminationService illuminationService;
 
+    /**
+     * 가장 최신 조도를 단일로 조회하는 api
+     *
+     * @return 최신 조도 응답
+     */
     @GetMapping
     public ResponseEntity<Illumination> getIllumination() {
         Illumination illumination = illuminationService.getIllumination();
@@ -26,6 +37,11 @@ public class IlluminationRestController {
         return ResponseEntity.ok(illumination);
     }
 
+    /**
+     * 당일 0시부터 현재 시간까지 1시간 주기로 조도를 조회하는 api
+     *
+     * @return 당일 조도 리스트 응답
+     */
     @GetMapping("/day")
     public ResponseEntity<List<IlluminationMaxMinDaily>> getDailyIlluminations() {
         List<IlluminationMaxMinDaily> illuminations = illuminationService.getDailyIlluminations();
@@ -33,6 +49,11 @@ public class IlluminationRestController {
         return ResponseEntity.ok(illuminations);
     }
 
+    /**
+     * 일주일 전부터 오늘까지 하루 단위로 조도를 조회하는 api
+     *
+     * @return 일주일 조도 리스트 응답
+     */
     @GetMapping("/week")
     public ResponseEntity<List<IlluminationMaxMinWeekly>> getWeeklyIlluminations() {
         List<IlluminationMaxMinWeekly> illuminations = illuminationService.getWeeklyIlluminations();
@@ -40,6 +61,11 @@ public class IlluminationRestController {
         return ResponseEntity.ok(illuminations);
     }
 
+    /**
+     * 한달 전부터 오늘까지 하루 단위로 조도를 조회하는 api
+     *
+     * @return 한달 조도 리스트 응답
+     */
     @GetMapping("/month")
     public ResponseEntity<List<IlluminationMaxMinMonthly>> getMonthlyIlluminations() {
         List<IlluminationMaxMinMonthly> illuminations = illuminationService.getMonthlyIlluminations();
