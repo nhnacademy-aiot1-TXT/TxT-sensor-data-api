7db2d97f4a2fd880572de5608ddaa98f0066ff2c
pass0210 <pass0210@gmail.com>
2024-04-17 22:03:25 +0900

    Docs: VocService JavaDoc 작성
    VocService interface 설명 작성

--- src/main/java/com/nhnacademy/sensordata/service/VocService.java
+++ src/main/java/com/nhnacademy/sensordata/service/VocService.java
@@ -2,6 +2,12 @@ package com.nhnacademy.sensordata.service;
 
 import com.nhnacademy.sensordata.entity.voc.Voc;
 
+/**
+ * voc 서비스 interface
+ *
+ * @author parksangwon
+ * @version 1.0.0
+ */
 public interface VocService {
     Voc getVoc();
 }
