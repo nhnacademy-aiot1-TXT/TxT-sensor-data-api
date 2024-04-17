e754350cfc2d30517d8c20d63acdbf39077b3c36
pass0210 <pass0210@gmail.com>
2024-04-17 21:47:47 +0900

    Docs: IlluminationService JavaDoc 작성
    ### 작업 내용
    - IlluminationService interface 설명 작성
    - IlluminationServiceImpl class 설명 작성

--- src/main/java/com/nhnacademy/sensordata/service/IlluminationService.java
+++ src/main/java/com/nhnacademy/sensordata/service/IlluminationService.java
@@ -7,12 +7,38 @@ import com.nhnacademy.sensordata.entity.illumination.IlluminationMaxMinWeekly;
 
 import java.util.List;
 
+/**
+ * 조도 서비스 interface
+ *
+ * @author parksangwon
+ * @version 1.0.0
+ */
 public interface IlluminationService {
+    /**
+     * 조도 단일 조회 메서드
+     *
+     * @return 단일 조도
+     */
     Illumination getIllumination();
 
+    /**
+     * 일간 1시간 주기로 만들어진 조도 리스트 조회 메서드
+     *
+     * @return 일간 조도 리스트
+     */
     List<IlluminationMaxMinDaily> getDailyIlluminations();
 
+    /**
+     * 주간 하루 주기로 만들어진 조도 리스트 조회 메서드
+     *
+     * @return 주간 조도 리스트
+     */
     List<IlluminationMaxMinWeekly> getWeeklyIlluminations();
 
+    /**
+     * 월간 하루 주기로 만들어진 조도 리스트 조회 메서드
+     *
+     * @return 월간 조도 리스트
+     */
     List<IlluminationMaxMinMonthly> getMonthlyIlluminations();
 }
