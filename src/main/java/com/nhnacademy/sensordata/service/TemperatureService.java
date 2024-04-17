6f52f83292a35c5275ebda215650bebed905ccb6
pass0210 <pass0210@gmail.com>
2024-04-17 21:05:49 +0900

    Docs: TemperatureService JavaDoc 작성
    ### 작업 내용
    - TemperatureService interface 설명 작성
    - TemperatureServiceImpl class 설명 작성

--- src/main/java/com/nhnacademy/sensordata/service/TemperatureService.java
+++ src/main/java/com/nhnacademy/sensordata/service/TemperatureService.java
@@ -7,12 +7,38 @@ import com.nhnacademy.sensordata.entity.temperature.TemperatureMaxMinWeekly;
 
 import java.util.List;
 
+/**
+ * 온도 서비스 interface
+ *
+ * @author parksangwon
+ * @version 1.0.0
+ */
 public interface TemperatureService {
+    /**
+     * 온도 단일 조회 메서드
+     *
+     * @return 단일 온도
+     */
     Temperature getTemperature();
 
+    /**
+     * 일간 1시간 주기로 만들어진 온도 리스트 조회 메서드
+     *
+     * @return 일간 온도 리스트
+     */
     List<TemperatureMaxMinDaily> getDailyTemperatures();
 
+    /**
+     * 주간 하루 주기로 만들어진 온도 리스트 조회 메서드
+     *
+     * @return 주간 온도 리스트
+     */
     List<TemperatureMaxMinWeekly> getWeeklyTemperatures();
 
+    /**
+     * 월간 하루 주기로 만들어진 온도 리스트 조회 메서드
+     *
+     * @return 월간 온도 리스트
+     */
     List<TemperatureMaxMinMonthly> getMonthlyTemperatures();
 }
