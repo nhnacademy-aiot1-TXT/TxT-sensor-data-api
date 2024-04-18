0764b46e5909b8e442421e741e036a07f5033010
jongsikk <xxx_@kakao.com>
2024-04-18 13:32:39 +0900

    Refactor: 예외처리 Test code 추가

--- src/test/java/com/nhnacademy/sensordata/service/PeopleCountServiceImplTest.java
+++ src/test/java/com/nhnacademy/sensordata/service/PeopleCountServiceImplTest.java
@@ -3,6 +3,7 @@ package com.nhnacademy.sensordata.service;
 import com.influxdb.client.InfluxDBClient;
 import com.influxdb.client.QueryApi;
 import com.nhnacademy.sensordata.entity.people_count.PeopleCount;
+import com.nhnacademy.sensordata.exception.PeopleCountNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +11,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.time.Instant;
+import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
@@ -44,5 +45,13 @@ class PeopleCountServiceImplTest {
                 () -> assertEquals(peopleCount.getInCount(), resultPeopleCount.getInCount()),
                 () -> assertEquals(peopleCount.getOutCount(), resultPeopleCount.getOutCount())
         );
+    }
+
+    @Test
+    void getPeopleCountException() {
+        given(influxDBClient.getQueryApi()).willReturn(queryApi);
+        given(queryApi.query(anyString(), eq(PeopleCount.class))).willReturn(Collections.emptyList());
+
+        assertThrows(PeopleCountNotFoundException.class, () -> peopleCountService.getPeopleCount());
     }
 }
