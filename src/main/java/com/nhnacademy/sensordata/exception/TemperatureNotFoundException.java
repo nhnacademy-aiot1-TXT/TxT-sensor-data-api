74053c0ae53b85793c6bbfdc265a2190e348c156
pass0210 <pass0210@gmail.com>
2024-04-18 14:39:01 +0900

    TemperatureNotFoundException 수정
    JavaDoc 추가

--- src/main/java/com/nhnacademy/sensordata/exception/TemperatureNotFoundException.java
+++ src/main/java/com/nhnacademy/sensordata/exception/TemperatureNotFoundException.java
@@ -1,9 +1,20 @@
 package com.nhnacademy.sensordata.exception;
 
+/**
+ * 온도를 찾을 수 없을 때 발생하는 예외 class
+ */
 public class TemperatureNotFoundException extends RuntimeException {
+    /**
+     * 온도 예외 기본 생성자
+     */
     public TemperatureNotFoundException() {
     }
 
+    /**
+     * 온도 예외에 메시지가 포함되는 생성자
+     *
+     * @param message 예외 메시지
+     */
     public TemperatureNotFoundException(String message) {
         super(message);
     }
