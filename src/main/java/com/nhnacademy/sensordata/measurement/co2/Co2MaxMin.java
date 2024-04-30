ff247aeb2991824a436c6cc93fb0efe210127622
jongsikk <xxx_@kakao.com>
2024-04-30 10:19:14 +0900

    Chore: .gitignore 수정 및 Measurement class 정리
    .gitignore -> .DS_Store 추가
    Daily, Weekly, Monthly -> MaxMin 통합

--- src/main/java/com/nhnacademy/sensordata/measurement/co2/Co2MaxMin.java
+++ src/main/java/com/nhnacademy/sensordata/measurement/co2/Co2MaxMin.java
@@ -1,7 +1,6 @@
 package com.nhnacademy.sensordata.measurement.co2;
 
 import com.influxdb.annotations.Column;
-import com.influxdb.annotations.Measurement;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +9,7 @@ import lombok.Setter;
 import java.time.Instant;
 
 /**
- * co2 월별 조회 measurement class
+ * co2 일별 조회 measurement class
  *
  * @author jongsikk
  * @version 1.0.0
@@ -19,8 +18,7 @@ import java.time.Instant;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Measurement(name = "co2_daily")
-public class Co2MaxMinMonthly {
+public class Co2MaxMin {
     @Column(name = "time")
     private Instant time;
     @Column(name = "max_co2")
--- src/main/java/com/nhnacademy/sensordata/measurement/illumination/IlluminationMaxMin.java
+++ src/main/java/com/nhnacademy/sensordata/measurement/illumination/IlluminationMaxMin.java
@@ -1,7 +1,6 @@
 package com.nhnacademy.sensordata.measurement.illumination;
 
 import com.influxdb.annotations.Column;
-import com.influxdb.annotations.Measurement;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +9,7 @@ import java.time.Instant;
 
 
 /**
- * 조도 월별 조회 measurement class
+ * 조도 일별 조회 measurement class
  *
  * @author parksangwon
  * @version 1.0.0
@@ -18,8 +17,7 @@ import java.time.Instant;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Measurement(name = "illumination_daily")
-public class IlluminationMaxMinMonthly {
+public class IlluminationMaxMin {
     @Column(name = "time")
     private Instant time;
     @Column(name = "max_illumination")
